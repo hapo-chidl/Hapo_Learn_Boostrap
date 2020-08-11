3cfe15d4b8743c6e838c565b3b4bc30115caca17
linhchi2406 <linhchi24060508@gmail.com>
2020-08-11 10:33:36 +0700

    Add file feedback

--- JS/design.js
+++ JS/design.js
@@ -11,3 +11,33 @@ function messButton() {
     var closebtns = document.getElementById("box-chat");
     closebtns.style.display = "none";
   }
+
+$(document).ready(function(){
+  $('#header-button').click(function() {
+      $('#icon').toggleClass('fa-times');
+      $('#icon').toggleClass('fa-bars');
+  });
+
+  $('.fb-content').slick({
+    centerMode: false,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed : 2000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    prevArrow: $('.next-left'),
+    nextArrow: $('.next-right'),
+  
+      responsive: [
+        {
+          breakpoint: 980,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+});
